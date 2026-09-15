eaa7a00fcacf83c5667dce0e9f158b6635ff7bf4
Muhammad-Kashif5 <46m.kashifkhan@gmail.com>
2026-09-15 18:47:50 +0500

    This is my first assignment of JS

---
+++ app.js
@@ -0,0 +1,24 @@
+/*Basic Practice  */
+// alert("Hello World")
+// confirm("Hello World")
+// document.write("Hello World")
+console.log("Hello world");
+/* Variable Practice */
+
+var student_name = "Trump"
+console.log(student_name);
+
+var student_name = "John"
+
+let course = "Full-Stack Web Development"
+// console.log(course);
+course = "MERN-Stack Web Development" // can be  reassign but cannot be redeclare
+console.log(course);
+
+const batch_number = 822217;
+// batch_number = 822218; cannot be redeclare and reassign
+// console.log(batch_number);
+
+console.log(batch_number);
+document.write(batch_number)
+
